3631fce36c2d4ade9962c3338df8042c29379425
Matías Marcó del Pont <matiasmdelp@gmail.com>
2022-10-28 15:30:57 -0300

    Rewrite arange as a generator

--- lib/index.ts
+++ lib/index.ts
@@ -1,23 +1,25 @@
-export interface LazyArray<T> {
-  toArray: () => T[]
+export function arange(stop: number): Iterable<number>
+export function arange(start: number, stop: number): Iterable<number>
+export function arange(start: number, stop: number, step: number): Iterable<number>
+export function arange(_start: number, _stop?: number, step: number = 1): Iterable<number> {
+  if (step === 0) {
+    throw new RangeError('Invalid arange step')
+  }
 
-  get length(): number
-}
-
-export function arange(stop: number): LazyArray<number>
-export function arange(start: number, stop: number): LazyArray<number>
-export function arange(start: number, stop: number, step: number): LazyArray<number>
-export function arange(_start: number, _stop?: number, step: number = 1): LazyArray<number> {
   const start = _stop !== undefined ? _start : 0
   const stop = _stop !== undefined ? _stop : _start
 
   const length = Math.max(0, Math.ceil((stop - start) / step))
 
   return {
-    toArray: () => new Array(length).fill(0).map((_, i) => start + step * i),
-
-    get length() {
-      return this.toArray().length
+    [Symbol.iterator]: function * () {
+      let x = start
+      let i = 0
+      while (i < length) {
+        yield x
+        x += step
+        i += 1
+      }
     }
   }
 }
