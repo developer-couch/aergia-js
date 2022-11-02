419877bc392abe32e5b17595e6f271eccaf3e82c
Matías Marcó del Pont <matiasmdelp@gmail.com>
2022-11-02 18:04:58 -0300

    Iterator recursive implementation of lazy operations. Tests for arange.

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,96 @@
-export function arange(stop: number): Iterable<number>
-export function arange(start: number, stop: number): Iterable<number>
-export function arange(start: number, stop: number, step: number): Iterable<number>
-export function arange(_start: number, _stop?: number, step: number = 1): Iterable<number> {
-  if (step === 0) {
-    throw new RangeError('Invalid arange step')
+export function shift<T>(iterator: Iterator<T>, n: number = 0): IteratorResult<T> {
+  const result = iterator.next()
+  if (n < 1) {
+    return result
   }
+  return shift(iterator, n - 1)
+}
 
-  const start = _stop !== undefined ? _start : 0
-  const stop = _stop !== undefined ? _stop : _start
+export function reduce<T, U>(iterator: Iterator<T>, reducer: (value: T, result: U) => U, initialValue: U, n: number = Infinity, stop?: (value: T) => boolean): U {
+  const { value, done } = shift(iterator)
+  if (done === true) {
+    return initialValue
+  }
+  if (n < 2 || stop?.(value) === true) {
+    return reducer(value, initialValue)
+  }
+  return reduce(iterator, reducer, reducer(value, initialValue), n - 1)
+}
 
-  const length = Math.max(0, Math.ceil((stop - start) / step))
+export function every<T>(iterator: Iterator<T>, test: (value: T) => boolean, n: number = Infinity): boolean {
+  return reduce(iterator, (value, result) => test(value) && result, true, n, (value) => !test(value))
+}
+
+export function some<T>(iterator: Iterator<T>, test: (value: T) => boolean, n: number = Infinity): boolean {
+  return !every(iterator, (value) => !test(value), n)
+}
+
+export function take<T>(iterator: Iterator<T>, n: number = Infinity): T[] {
+  return reduce(iterator, (value, result) => { result.push(value); return result }, [] as T[], n)
+}
+
+export function count<T>(iterator: Iterator<T>, test: (value: T) => boolean, n: number = Infinity): number {
+  return reduce(iterator, (value, result) => result + (test(value) ? 1 : 0), 0, n)
+}
+
+export function min<T>(iterator: Iterator<T>, lessThan: (value: T, otherValue: T) => boolean, n: number = Infinity): T {
+  const { value: first, done } = shift(iterator)
+  if (done === true) {
+    throw TypeError('Empty iterator')
+  }
+  return reduce(iterator, (value, otherValue) => lessThan(value, otherValue) ? value : otherValue, first, n)
+}
+
+export function max<T>(iterator: Iterator<T>, lessThan: (value: T, otherValue: T) => boolean, n: number = Infinity): T {
+  return min(iterator, (value, otherValue) => lessThan(otherValue, value), n)
+}
+
+export function sort<T>(iterator: Iterator<T>, lessThan: (value: T, otherValue: T) => boolean, n: number = Infinity): T[] {
+  // TODO: make it faster by taking while sorting with a BST.
+  // Every operation should use reduce for performance optimization
+  return take(iterator, n).sort((value, otherValue) => lessThan(value, otherValue) ? -1 : 1)
+}
+
+export function map<T, U>(iterator: Iterator<T>, mapper: (value: T) => U): Iterator<U> {
+  return {
+    next(): IteratorResult<U> {
+      const { value, done } = shift(iterator)
+      if (done === true) {
+        return { value, done }
+      }
+      return { value: mapper(value), done }
+    }
+  }
+}
+
+export function filter<T, U extends T>(iterator: Iterator<T>, test: (value: T) => value is U): Iterator<U>
+export function filter<T>(iterator: Iterator<T>, test: (value: T) => boolean): Iterator<T>
+export function filter<T>(iterator: Iterator<T>, test: (value: T) => boolean): Iterator<T> {
+  return {
+    next(): IteratorResult<T> {
+      const { value, done } = shift(iterator)
+      if (done === true || test(value)) {
+        return { value, done }
+      }
+      return this.next()
+    }
+  }
+}
+
+export function arange(stop: number): Iterator<number>
+export function arange(start: number, stop: number): Iterator<number>
+export function arange(start: number, stop: number, step: number): Iterator<number>
+export function arange(_start: number, _stop?: number, step: number = 1): Iterator<number> {
+  const start = _stop === undefined ? 0 : _start
+  const stop = _stop === undefined ? _start : _stop
 
   return {
-    [Symbol.iterator]: function * () {
-      let x = start
-      let i = 0
-      while (i < length) {
-        yield x
-        x += step
-        i += 1
+    next(): IteratorResult<number> {
+      if (((step > 0) && start >= stop) || ((step < 0) && start <= stop)) {
+        return { value: undefined, done: true }
       }
+      this.next = arange(start + step, stop, step).next
+      return { value: start }
     }
   }
 }
--- tests/index.ts
+++ tests/index.ts
@@ -1,68 +1,68 @@
 import assert from 'node:assert'
 import { describe, it } from 'node:test'
 
-import { arange } from '../lib/'
+import { arange, take } from '../lib/'
 
 describe('arange', function () {
   describe('only stop', function () {
     it('returns an empty lazy array when stop is 0', function () {
-      assert.deepStrictEqual([...arange(0)], [])
+      assert.deepStrictEqual(take(arange(0)), [])
     })
 
     it('returns consecutive numbers from 0 to stop - 1 when stop is a positive integer', function () {
-      assert.deepStrictEqual([...arange(3)], [0, 1, 2])
+      assert.deepStrictEqual(take(arange(3)), [0, 1, 2])
     })
 
     it('returns an empty lazy array when stop is negative', function () {
-      assert.deepStrictEqual([...arange(-3)], [])
+      assert.deepStrictEqual(take(arange(-3)), [])
     })
 
     it('returns consecutive numbers from 0 to floor(stop) when stop is not an integer', function () {
-      assert.deepStrictEqual([...arange(3.3)], [0, 1, 2, 3])
+      assert.deepStrictEqual(take(arange(3.3)), [0, 1, 2, 3])
     })
   })
 
   describe('start and stop', function () {
     it('returns an empty lazy array when start is equal to stop', function () {
-      assert.deepStrictEqual([...arange(2, 2)], [])
+      assert.deepStrictEqual(take(arange(2, 2)), [])
     })
 
     it('returns consecutive numbers from start to stop - 1 when start and stop are positive integers with stop > start', function () {
-      assert.deepStrictEqual([...arange(-2, 3)], [-2, -1, 0, 1, 2])
+      assert.deepStrictEqual(take(arange(-2, 3)), [-2, -1, 0, 1, 2])
     })
 
     it('returns an empty array when stop < start', function () {
-      assert.deepStrictEqual([...arange(3, 1)], [])
+      assert.deepStrictEqual(take(arange(3, 1)), [])
     })
 
     it('returns numbers from start to stop with a step of 1 when start and stop are not integers', function () {
-      assert.deepStrictEqual([...arange(1.1, 3.3)], [1.1, 2.1, 3.1])
+      assert.deepStrictEqual(take(arange(1.1, 3.3)), [1.1, 2.1, 3.1])
     })
 
     it('never includes stop when start and stop are not integers', function () {
-      assert.deepStrictEqual([...arange(1.1, 3.1)], [1.1, 2.1])
+      assert.deepStrictEqual(take(arange(1.1, 3.1)), [1.1, 2.1])
     })
   })
 
   describe('start, stop and step', function () {
     it('throws when step is 0', function () {
-      assert.throws(() => { arange(1, 3, 0) })
+      assert.throws(() => { take(arange(1, 3, 0)) })
     })
 
     it('returns numbers from start to stop - 1 with step when start and stop are positive integers with stop > start', function () {
-      assert.deepStrictEqual([...arange(-2, 4, 1.5)], [-2, -0.5, 1, 2.5])
+      assert.deepStrictEqual(take(arange(-2, 4, 1.5)), [-2, -0.5, 1, 2.5])
     })
 
     it('can go backwards if stop < start and step < 0', function () {
-      assert.deepStrictEqual([...arange(3, -2, -2)], [3, 1, -1])
+      assert.deepStrictEqual(take(arange(3, -2, -2)), [3, 1, -1])
     })
 
     it('returns an empty lazy array when step is < 0 and stop > start', function () {
-      assert.deepStrictEqual([...arange(1, 3, -1)], [])
+      assert.deepStrictEqual(take(arange(1, 3, -1)), [])
     })
 
     it('returns an empty lazy array when step is > 0 and stop < start', function () {
-      assert.deepStrictEqual([...arange(3, 1, 1)], [])
+      assert.deepStrictEqual(take(arange(3, 1, 1)), [])
     })
   })
 })
